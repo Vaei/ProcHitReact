17143fe106d5c0f7a83e6612e2a68dad3f3eea9b
Jared Taylor <jaredtaylor.99@gmail.com>
2024-10-29 22:07:52 +1300

    Add BumpPawn profile

--- Source/ProcHitReact/Private/HitReactTags.cpp
+++ Source/ProcHitReact/Private/HitReactTags.cpp
@@ -9,6 +9,7 @@ namespace FHitReactTags
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(HitReact_Profile_Default_NoArms, "HitReact.Profile.Default.NoArms", "Blend weight for arms is ignored");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(HitReact_Profile_Default_NoLegs, "HitReact.Profile.Default.NoLegs", "Blend weight for legs is ignored");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(HitReact_Profile_Default_NoLimbs, "HitReact.Profile.Default.NoLimbs", "Blend weight for arms and legs are ignored");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(HitReact_Profile_BumpPawn, "HitReact.Profile.BumpPawn", "Designed for bumping into other pawns, a very slow and lazy hit react");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(HitReact_Profile_TakeShot, "HitReact.Profile.TakeShot", "Designed for rapid re-application of hit reacts, such as taking multiple shots");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(HitReact_Profile_TakeShot_NoArms, "HitReact.Profile.TakeShot.NoArms", "Designed for rapid re-application of hit reacts, such as taking multiple shots, while ignoring arm bones");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(HitReact_Profile_Flop, "HitReact.Profile.Flop", "Makes the character as floppy as possible, primarily for testing purposes");
--- Source/ProcHitReact/Private/Params/HitReactParams.cpp
+++ Source/ProcHitReact/Private/Params/HitReactParams.cpp
@@ -13,6 +13,7 @@ TMap<FGameplayTag, FHitReactProfile> FHitReactBuiltInProfiles::GetBuiltInProfile
 	FHitReactProfile NoArms = {};
 	FHitReactProfile NoLegs = {};
 	FHitReactProfile NoLimbs = {};
+	FHitReactProfile BumpPawn = {};
 	FHitReactProfile TakeShot = {};
 	FHitReactProfile TakeShotNoArms = {};
 	FHitReactProfile Flop = {};
@@ -23,12 +24,14 @@ TMap<FGameplayTag, FHitReactProfile> FHitReactBuiltInProfiles::GetBuiltInProfile
 	Flop.DefaultBoneApplyParams.PhysicsBlendParams.BlendHoldTime = 0.2f;
 	Flop.DefaultBoneApplyParams.DecayExistingPhysics = 0.05f;
 
+	// Default profile
 	Default.OverrideBoneParams = {
 		{ TEXT("neck_01"), { true, false, 0.f, 0.3f } },
 		{ TEXT("clavicle_l"), { true, false, 0.f, 0.2f } },
 		{ TEXT("clavicle_r"), { true, false, 0.f, 0.2f } }
 	};
 
+	// No arms profile
 	NoArms = Default;
 	NoArms.OverrideBoneParams = {
 		{ TEXT("neck_01"), { true, false, 0.f, 0.3f } },
@@ -36,6 +39,7 @@ TMap<FGameplayTag, FHitReactProfile> FHitReactBuiltInProfiles::GetBuiltInProfile
 		{ TEXT("clavicle_r"), { true, true, 0.f, 0.f } }
 	};
 
+	// No legs profile
 	NoLegs = Default;
 	NoLegs.OverrideBoneParams = {
 		{ TEXT("neck_01"), { true, false, 0.f, 0.3f } },
@@ -43,6 +47,7 @@ TMap<FGameplayTag, FHitReactProfile> FHitReactBuiltInProfiles::GetBuiltInProfile
 		{ TEXT("thigh_r"), { true, true, 0.f, 0.f } }
 	};
 
+	// No limbs profile
 	NoLimbs = Default;
 	NoLimbs.OverrideBoneParams = {
 		{ TEXT("neck_01"), { true, false, 0.f, 0.3f } },
@@ -52,6 +57,20 @@ TMap<FGameplayTag, FHitReactProfile> FHitReactBuiltInProfiles::GetBuiltInProfile
 		{ TEXT("thigh_r"), { true, true, 0.f, 0.f } }
 	};
 
+	// Bump pawn profile
+	BumpPawn = Default;
+	BumpPawn.DefaultBoneApplyParams.PhysicsBlendParams.BlendHoldTime = 0.2f;
+	BumpPawn.DefaultBoneApplyParams.PhysicsBlendParams.BlendOut.BlendTime = 0.7f;
+	BumpPawn.DefaultBoneApplyParams.PhysicsBlendParams.DecayTime = 0.2f;
+	BumpPawn.DefaultBoneApplyParams.PhysicsBlendParams.MaxAccumulatedDecayTime = 0.45f;
+	BumpPawn.DefaultBoneApplyParams.PhysicsBlendParams.BlendIn.BlendOption = EAlphaBlendOption::CircularIn;
+	BumpPawn.DefaultBoneApplyParams.PhysicsBlendParams.BlendOut.BlendOption = EAlphaBlendOption::CircularOut;
+	BumpPawn.DefaultBoneApplyParams.MaxBlendWeight = 0.3f;
+	BumpPawn.DefaultBoneApplyParams.Cooldown = 0.35f;
+	BumpPawn.DefaultBoneApplyParams.DecayExistingPhysics = 0.1f;
+	BumpPawn.OverrideBoneParams = {};
+
+	// Take shot profile
 	TakeShot = Default;
 	TakeShot.DefaultBoneApplyParams.MaxBlendWeight = 0.2f;
 	TakeShot.DefaultBoneApplyParams.PhysicsBlendParams.DecayTime = 0.15f;
@@ -64,7 +83,8 @@ TMap<FGameplayTag, FHitReactProfile> FHitReactBuiltInProfiles::GetBuiltInProfile
 		{ TEXT("clavicle_l"), { true, false, 0.f, 0.12f } },
 		{ TEXT("clavicle_r"), { true, false, 0.f, 0.12f } }
 	};
-	
+
+	// Take shot no arms profile
 	TakeShotNoArms = TakeShot;
 	TakeShotNoArms.OverrideBoneParams = {
 		{ TEXT("neck_01"), { true, false, 0.f, 0.15f } },
@@ -78,10 +98,12 @@ TMap<FGameplayTag, FHitReactProfile> FHitReactBuiltInProfiles::GetBuiltInProfile
 		{ FHitReactTags::HitReact_Profile_Default_NoArms, NoArms },
 		{ FHitReactTags::HitReact_Profile_Default_NoLegs, NoLegs },
 		{ FHitReactTags::HitReact_Profile_Default_NoLimbs, NoLimbs },
+		{ FHitReactTags::HitReact_Profile_BumpPawn, BumpPawn },
 		{ FHitReactTags::HitReact_Profile_TakeShot, TakeShot },
 		{ FHitReactTags::HitReact_Profile_TakeShot_NoArms, TakeShotNoArms },
 		{ FHitReactTags::HitReact_Profile_Flop, Flop }
 	};
-		
+
+	// Return the profiles
 	return Profiles;
 }
